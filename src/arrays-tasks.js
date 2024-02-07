5b65d9317f09139f6667fa6c5ff314f0b7307504
D4n1el13and37 <daniel837@mail.ru>
2024-02-07 20:52:04 +0300

    feat: solve createNDimensionalArray task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -272,8 +272,13 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  const answer = Array.from({ length: n - 1 });
+  const zeroFilled = Array(size).fill(0);
+
+  return answer.reduce((initial) => {
+    return initial.map(() => initial);
+  }, zeroFilled);
 }
 
 /**
